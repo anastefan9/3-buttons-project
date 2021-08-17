e59331f75bc0d32b4a2acdc803c4f5c285f914e2
anastefan9 <ana.stefan1000@gmail.com>
2021-08-17 17:31:26 +0300

    Second commit

--- myScript.js
+++ myScript.js
@@ -1,42 +1,24 @@
-var countButton1 = 0;
-var countNextButton = 0;
-function button1Function() {
-	document.getElementById('loserButton2').style.display = 'none';
-	document.getElementById('loserButton3').style.display = 'none';
-	document.getElementById('winnerButton').style.display = 'block';
-	++countButton1;
-	if (countButton1 > 0 && document.getElementById('roundMessage').innerHTML != 'Runda 3') {
-		document.getElementById('nextRoundButton').style.display = 'block';
-	}
-	if (document.getElementById('roundMessage').innerHTML == 'Runda 3') {
-		document.getElementById('lastText').style.display = 'block';
-	}
-}
-function button2Function() {
-	document.getElementById('loserButton2').style.display = 'block';
-	document.getElementById('loserButton3').style.display = 'none';
-	document.getElementById('winnerButton').style.display = 'none';
-}
-function button3Function() {
-	document.getElementById('loserButton3').style.display = 'block';
-	document.getElementById('loserButton2').style.display = 'none';
-	document.getElementById('winnerButton').style.display = 'none';
-}
+var win_nr = getRandomNumber(1, 3);
+var winVar = document.getElementById("winMessage");
+var loseVar= document.getElementById("loseMessage");
 
-function nextButtonFunction() {
-	document.getElementById('roundMessage').innerHTML = 'Runda 2';
-	document.getElementById('loserButton3').innerHTML = 'Butonul 1 nu este castigator';
-	document.getElementById('loserButton2').style.display = 'none';
-	document.getElementById('loserButton3').style.display = 'none';
-	document.getElementById('winnerButton').style.display = 'none';
-	document.getElementById('Button3').onclick = function() {button1Function();};
-	document.getElementById('Button1').onclick = function() {button3Function();};
-	document.getElementById('nextRoundButton').style.display = 'none';
-	++countNextButton;
-	if (countNextButton > 1) {
-		document.getElementById('roundMessage').innerHTML = 'Runda 3';
-		document.getElementById('Button2').onclick = function() {button1Function();};
-		document.getElementById('Button3').onclick = function() {button2Function();};
-		document.getElementById('loserButton2').innerHTML = 'Butonul 3 nu este castigator';
+function create3Buttons() {
+	for (var i = 1; i <= 3; ++i) {
+		var container = document.getElementById("box");
+		var button = document.createElement("BUTTON");
+	    container.appendChild(button);
+	    button.innerHTML = "Buton" + " " + i;
+	    if (i == win_nr) {
+	    	button.onclick = function() {getMessage(winVar, loseVar)};
+	    } else {
+	    	button.onclick = function() {getMessage(loseVar, winVar)};
+	    }
 	}
+}
+function getRandomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function getMessage(win, lose) {
+	win.style.display = "block";
+	lose.style.display = "none";
 }
